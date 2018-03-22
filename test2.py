2aad6781cf4d94e116d13150dbce966020297b69
ashleshbhat <ashleshbhat@gmail.com>
2018-03-22 13:18:54 +0100

    counting working

--- test2.py
+++ test2.py
@@ -3,14 +3,32 @@ import pyspark
 from pyspark import SparkContext
 from pyspark import SparkConf
 import time
+from functools import reduce
+import re
 
 t1 = time.time()
-logFilename = "README.md"  
+logFilename = "output.txt"  
 logFile = open(logFilename)
-logFile = list(logFile)
-logData = list(map(lambda line: line.split(' ') ,logFile))
-#logData = (map(lambda line: line.split(' '),logData))
-print(logData)
+logData = logFile.read().replace('\n','').replace('-', ' ')
+logsplit = logData.split()
+
+def f1(listarg,listarg2):
+    if listarg2[0] in listarg:
+        pos = listarg.index(listarg2[0])
+        listnew = list(listarg)
+        listnew[pos+1] +=1
+        listarg = tuple(listnew)
+    else:
+        listarg +=listarg2
+       
+    return listarg 
+
+CountMap = tuple(map(lambda word: (word,1), logsplit))
+Reduced =tuple(reduce(lambda x,y: f1(x,y), CountMap))
+
+print(Reduced)  
+
 t2 = time.time()
 
+#print(Reduced)
 print (t2-t1)
